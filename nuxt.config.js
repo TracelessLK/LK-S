427450741e6d52505e9679c50f84c62c28c92edb
catch <catchspirit37@gmail.com>
2020-09-22 21:59:34 +0800

    z - Tue Sep 22 2020 21:59:28 GMT+0800 (China Standard Time)

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,5 +54,9 @@ module.exports = {
   ],
   plugins: [
     { src: '~plugins/iview', ssr: true }
-  ]
+  ],
+  server: {
+    port: 80, // default: 3000
+    host: '0.0.0.0' // default: localhost
+  }
 }
